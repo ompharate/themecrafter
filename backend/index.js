f197cffb0ab30356a0db1caed6316341584999a0
om pharate <72200400+ompharate@users.noreply.github.com>
2024-06-07 18:55:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://opthemestore.vercel.app,
   methods: ["GET", "POST"],
   credentials: true,
 };
